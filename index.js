49fc031dcd5e32fbb34c4904ce7b44976aa8ebbe
Mohammad Rahman <me@shadikur.com>
2023-05-07 22:13:57 +0200

    Coffe Add request updated

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    app.post("/coffee", (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -27,7 +33,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
